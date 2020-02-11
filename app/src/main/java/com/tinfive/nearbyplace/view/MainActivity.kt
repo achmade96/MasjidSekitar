280acb1a9fabc650fad979f07e211d0a2ef0e0cd
Ahmad Mujahid <49054944+achmade96@users.noreply.github.com>
2020-02-11 22:19:49 +0700

    confused

--- app/src/main/java/com/tinfive/nearbyplace/view/MainActivity.kt
+++ app/src/main/java/com/tinfive/nearbyplace/view/MainActivity.kt
@@ -6,12 +6,14 @@ import android.content.pm.PackageManager
 import android.location.Location
 import android.os.Build
 import android.os.Bundle
+import android.os.Handler
 import android.os.Looper
 import android.view.Menu
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SearchView
 import androidx.core.app.ActivityCompat
@@ -19,6 +21,7 @@ import androidx.core.content.ContextCompat
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.gms.location.*
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -29,28 +32,32 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 import com.tinfive.nearbyplace.R
+import com.tinfive.nearbyplace.adapter.FasilitasAdapter
 import com.tinfive.nearbyplace.adapter.ListMasjidAdapter
-import com.tinfive.nearbyplace.fragment.FasilitasFragment
+import com.tinfive.nearbyplace.adapter.MasjidFasilitasAdapter
 import com.tinfive.nearbyplace.model.FasilitasString
 import com.tinfive.nearbyplace.model.Masjid
 import com.tinfive.nearbyplace.networks.EndPoint.MY_PERMISSION_CODE
+import com.tinfive.nearbyplace.networks.MasjidApi
+import com.tinfive.nearbyplace.networks.RetrofitClient
 import com.tinfive.nearbyplace.utils.EqualSpacingItemDecoration
 import com.tinfive.nearbyplace.utils.MapsUtils
 import com.tinfive.nearbyplace.utils.MapsUtils.Companion.getUrl
-import com.tinfive.nearbyplace.viewmodel.ListViewModel
 import com.tinfive.nearbyplace.viewmodel.MapActivityModel
+import com.tinfive.nearbyplace.viewmodel.MasjidListViewModel
+import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.item_bottom_sheet.*
 
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
-    private var valueSelected : Int = 0
-    private var valueRespond : String =  ""
+    //fasilitas
     private var itung: Int = 0
-    lateinit var fasilitasModel: FasilitasFragment
-
     var fasilitasList: MutableList<FasilitasString> = mutableListOf()
+    internal lateinit var restApi: MasjidApi
+    private lateinit var masjidListFilter: MutableList<Masjid>
+
     //MAPS
     private lateinit var mMap: GoogleMap
     private lateinit var mLastLocation: Location
@@ -65,7 +72,7 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     lateinit var locationCallback: LocationCallback
 
     //List Masjid
-    lateinit var viewModel: ListViewModel
+    lateinit var viewModel: MasjidListViewModel
     private val masjidAdapter =
         ListMasjidAdapter(ArrayList())
 
@@ -79,10 +86,15 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        viewModel = ViewModelProvider(this)[ListViewModel::class.java]
-        mapsModel = ViewModelProvider(this).get(MapActivityModel::class.java)
-        viewModel.loadFasilitas()
+        restApi = RetrofitClient.getFasilitasList() //filterasi
 
+        Handler().post { fetchCategories() } //DAPETIN LIST FASILITAS MENGGUNAKAN API / RETROFIT
+
+        recycler_masjids.setHasFixedSize(true)
+        recycler_masjids.layoutManager = LinearLayoutManager(this)
+
+        viewModel = ViewModelProvider(this)[MasjidListViewModel::class.java]
+        mapsModel = ViewModelProvider(this).get(MapActivityModel::class.java)
 
 //        initBottom()
 
@@ -92,7 +104,7 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
                     println("SORT BELOM AKTIF")
                 }
                 R.id.btn_filter -> {
-                    showBottomSheetDialogFragment()
+                    showFilterDialog()
                 }
             }
             true
@@ -111,10 +123,6 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
             viewModel.refresh()
         }
 
-        //===========View======================
-        listError.visibility = View.GONE
-        //=====================================================
-
         //Request runtime permission
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             if (checkLocationPermission()) {
@@ -124,8 +132,190 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
             accessMapLiveLocation()
         }
         observeViewMapsModel() // ALL ABOUT GMAPS MARKER
-        initFilterData() //GET VALUE FILTER CHECKBOX
     }
+
+    //MELAKUKAN SHOW / POP-UP DIALOG DAN MENAMPILKAN LIST FASILITAS
+    private fun showFilterDialog() {
+
+        val alertDialog = AlertDialog.Builder(this@MainActivity)
+        alertDialog.setTitle("Pilih Fasilitas")
+
+        val inflater = this.layoutInflater
+        val filter_options_layout = inflater.inflate(R.layout.dialog_filter, null)
+
+        val recycler_options =
+            filter_options_layout.findViewById(R.id.recycler_options) as RecyclerView
+        recycler_options.setHasFixedSize(true)
+        recycler_options.layoutManager = LinearLayoutManager(this)
+        val adapter = FasilitasAdapter(baseContext, RetrofitClient.categories!!)
+        recycler_options.adapter = adapter
+
+        alertDialog.setView(filter_options_layout)
+
+        alertDialog.setNegativeButton("CANCEL") { dialogInterface, _ -> dialogInterface.dismiss() }
+        alertDialog.setPositiveButton("FILTER") { dialogInterface, _ ->
+
+
+            fetchFilterCategory(adapter.filterArray)
+        }
+
+        val dialog = alertDialog.create()
+
+        alertDialog.show()
+    }
+
+    //HASIL FASILITAS YANG TELAH DIPILIH AKAN TAMPIL DISINI
+    private fun fetchFilterCategory(filterArray: String) {
+        myCompositeDisposable.add(
+            restApi.getFilteredMasjid(filterArray) //MANGGIL POSTNYA DISINI
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({ filterCategories ->
+
+                    map.isVisible == true
+                    map.view?.visibility = View.GONE
+                    recycler_masjids.visibility = View.VISIBLE
+                    recycler_masjids.adapter = MasjidFasilitasAdapter(baseContext, filterCategories) // HASIL DILEMPAR KE ADAPTER YANG BERBEDA
+
+                },
+                    {
+                        Toast.makeText(baseContext, "FASILITAS", Toast.LENGTH_SHORT).show()
+                        recycler_masjids.visibility = View.VISIBLE
+                    })
+        )
+    }
+
+    //DAPETIN LIST FASILITAS DARI API
+    private fun fetchCategories() {
+        myCompositeDisposable.add(
+            restApi.fasilitasList
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({ categories ->
+                    RetrofitClient.categories = categories
+                },
+                    {
+                        Toast.makeText(baseContext, "", Toast.LENGTH_SHORT).show()
+                    })
+        )
+    }
+
+    //END FILTERASI
+
+    //VIEW MASJID TAMPILAN 2
+    private fun observeViewModel() {
+        viewModel.masjid.observe(this, Observer { masjid ->
+            masjid?.let {
+                recycler_masjids.visibility = View.VISIBLE
+                masjidAdapter.updateMasjid(it)
+                masjidAdapter.setOnItemClickListener(object :
+                    ListMasjidAdapter.OnItemClickListener {
+                    override fun onItemSelected(masjides: Masjid) {
+                        setOnClickItem(masjides.mosqueId, this@MainActivity)
+                    }
+                })
+            }
+        })
+
+        viewModel.masjidLoadError.observe(this, Observer { isError ->
+            isError?.let { listError.visibility = if (it) View.VISIBLE else View.GONE }
+        })
+
+        viewModel.loading.observe(this, Observer { isLoading ->
+            isLoading?.let {
+                loadingView.visibility = if (it) View.VISIBLE else View.GONE
+                if (it) {
+                    listError.visibility = View.GONE
+                    recycler_masjids.visibility = View.GONE
+                }
+            }
+        })
+
+    }
+
+    private fun initializeComponent() {
+        viewModel.refresh()
+
+        recycler_masjids.apply {
+            layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+            addItemDecoration(
+                EqualSpacingItemDecoration(
+                    12,
+                    EqualSpacingItemDecoration.HORIZONTAL
+                )
+            )
+            adapter = masjidAdapter
+        }
+    }
+
+    private fun setOnClickItem(mosqueId: Int, context: Context) {
+        val intent = Intent(context, DetailMasjid::class.java)
+        intent.putExtra("key", mosqueId)
+        context.startActivity(intent)
+    }
+
+    //SEARCH OPTIONS
+    @RequiresApi(Build.VERSION_CODES.HONEYCOMB)
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_toolbar, menu)
+
+        //getting the search view from the menu
+        val searchViewItem = menu!!.findItem(R.id.search)
+        //getting the search view
+        val searchView = searchViewItem.actionView as SearchView
+
+        searchView.setOnSearchClickListener {
+            if (map.isVisible == true) {
+                recycler_masjids.visibility = View.INVISIBLE
+                map.view?.visibility = View.GONE
+                changeLayoutView()
+            } else {
+                recycler_masjids.visibility = View.GONE
+                map.view?.visibility = View.GONE
+            }
+        }
+
+        searchView.setOnCloseListener {
+            if (map.isVisible == false) {
+                recycler_masjids.visibility = View.GONE
+                map.view?.visibility = View.VISIBLE
+                changeBackLayoutView()
+            }
+            observeViewModel()
+            false
+        }
+        searchView.maxWidth = Int.MAX_VALUE
+        searchView.queryHint = "Cari Masjid Yuk"
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextChange(query: String): Boolean {
+                recycler_masjids.visibility = View.GONE
+                masjidAdapter.filter.filter(query)
+//                println("DATA ACTIVITY $query")
+                return false
+            }
+
+            override fun onQueryTextSubmit(result: String): Boolean {
+                recycler_masjids.visibility = View.VISIBLE
+                masjidAdapter.filter.filter(result)
+                return false
+            }
+        })
+        return true
+    }
+
+    private fun changeBackLayoutView() {
+        val params: ViewGroup.LayoutParams = recycler_masjids.layoutParams
+        params.height = ViewGroup.LayoutParams.MATCH_PARENT
+        recycler_masjids.layoutParams = params
+    }
+
+    private fun changeLayoutView() {
+        val params: ViewGroup.LayoutParams = recycler_masjids.layoutParams
+        params.height = ViewGroup.LayoutParams.MATCH_PARENT
+        recycler_masjids.layoutParams = params
+    }
+    //END SEARCH OPTIONS
 
     private fun checkLocationPermission(): Boolean {
         if (ContextCompat.checkSelfPermission(
@@ -212,21 +402,6 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         })
     }
 
-    private fun initializeComponent() {
-        viewModel.refresh()
-
-        recycler_masjids.apply {
-            layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
-            addItemDecoration(
-                EqualSpacingItemDecoration(
-                    12,
-                    EqualSpacingItemDecoration.HORIZONTAL
-                )
-            )
-            adapter = masjidAdapter
-        }
-    }
-
     private fun accessMapLiveLocation() {
         buildLocationRequest()
         buildLocationCallback()
@@ -284,7 +459,6 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
                         Toast.LENGTH_SHORT
                     ).show()
                 }
-
             }
         }
     }
@@ -360,178 +534,20 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         mMap.uiSettings.isZoomControlsEnabled = true
     }
 
-    //VIEW MASJID TAMPILAN 2
-    private fun observeViewModel() {
-        viewModel.masjid.observe(this, Observer { masjid ->
-            masjid?.let {
-                recycler_masjids.visibility = View.VISIBLE
-                masjidAdapter.updateMasjid(it)
-                masjidAdapter.setOnItemClickListener(object : ListMasjidAdapter.OnItemClickListener {
-                    override fun onItemSelected(masjides: Masjid) {
-                        setOnClickItem(masjides.mosqueId, this@MainActivity)
-                    }
-                })
-            }
-        })
-
-        viewModel.masjidLoadError.observe(this, Observer { isError ->
-            isError?.let { listError.visibility = if (it) View.VISIBLE else View.GONE }
-        })
-
-        viewModel.loading.observe(this, Observer { isLoading ->
-            isLoading?.let {
-                loadingView.visibility = if (it) View.VISIBLE else View.GONE
-                if (it) {
-                    listError.visibility = View.GONE
-                    recycler_masjids.visibility = View.GONE
-                }
-            }
-        })
-
-    }
-
-    private fun setOnClickItem(mosqueId: Int, context: Context) {
-        val intent = Intent(context, DetailMasjid::class.java)
-        intent.putExtra("key", mosqueId)
-        context.startActivity(intent)
-    }
-
-    //SEARCH OPTIONS
-    @RequiresApi(Build.VERSION_CODES.HONEYCOMB)
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.menu_toolbar, menu)
-
-        //getting the search view from the menu
-        val searchViewItem = menu!!.findItem(R.id.search)
-        //getting the search view
-        val searchView = searchViewItem.actionView as SearchView
-
-        searchView.setOnSearchClickListener {
-            if (map.isVisible == true) {
-                recycler_masjids.visibility = View.INVISIBLE
-                map.view?.visibility = View.GONE
-                changeLayoutView()
-            } else {
-                recycler_masjids.visibility = View.GONE
-                map.view?.visibility = View.VISIBLE
-            }
-        }
-
-        searchView.setOnCloseListener {
-            if (map.isVisible == false) {
-                recycler_masjids.visibility = View.GONE
-                map.view?.visibility = View.VISIBLE
-                changeBackLayoutView()
-            }
-            observeViewModel()
-            false
-        }
-        searchView.maxWidth = Int.MAX_VALUE
-        searchView.queryHint = "Cari Masjid Yuk"
-        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
-            override fun onQueryTextChange(query: String): Boolean {
-                recycler_masjids.visibility = View.GONE
-                masjidAdapter.filter.filter(query)
-//                println("DATA ACTIVITY $query")
-                return false
-            }
-
-            override fun onQueryTextSubmit(result: String): Boolean {
-                recycler_masjids.visibility = View.VISIBLE
-                masjidAdapter.filter.filter(result)
-                return false
-            }
-        })
-        return true
-    }
-
-    private fun changeBackLayoutView() {
-        val params: ViewGroup.LayoutParams = recycler_masjids.layoutParams
-        params.height = ViewGroup.LayoutParams.MATCH_PARENT
-        recycler_masjids.layoutParams = params
-    }
-
-    private fun changeLayoutView() {
-        val params: ViewGroup.LayoutParams = recycler_masjids.layoutParams
-        params.height = ViewGroup.LayoutParams.MATCH_PARENT
-        recycler_masjids.layoutParams = params
-    }
-    //END SEARCH OPTIONS
-
-    //BOTTOM SHEET OPTIONS
-    private fun showBottomSheetDialogFragment() {
-        val bottomSheetFragment = FasilitasFragment.newInstance(fasilitasList)
-        bottomSheetFragment.show(supportFragmentManager, bottomSheetFragment.tag)
-        println("DATA SIZEE ${fasilitasList.size}")
-        println("DATAITUNG $itung")
-
-    }
-
-    //INI UNTUK MENGAKTIFKAN EFEK UP BUTTON NAV
-    /*private fun slideUpDownBottomSheet() {
-        if (bottomSheetBehavior.state != BottomSheetBehavior.STATE_EXPANDED) {
-            bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
-//            changeToTop()
-        } else {
-            bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
-//            changeToDown()
-        }
-    }*/
-
-    /*private fun changeToDown() {
-        setMargins(nav, 5, 0, 5, 20)
-    }
-
-    private fun changeToTop() {
-        setMargins(nav, 5, 0, 5, 900)
-    }*/
-
-    //END BOTTOM SHEET OPTIONS
-
     //Add back button
-    override fun onBackPressed() {
-        if (!searchView!!.isIconified) {
-            searchView!!.isIconified = true
-            return
-        }
+    override fun onSupportNavigateUp(): Boolean {
+
+        //back button to refresh activity masjid sekitar
+        startActivity(Intent(this@MainActivity, MainActivity::class.java))
+//        initializeComponent() //LAYOUTING LIST MASJID
+//        observeViewModel() //GET LIST MASJID
+
+        return true
     }
 
     override fun onStop() {
         myCompositeDisposable.clear()
         super.onStop()
-    }
-
-    //GET VALUE CHECKBOX FROM FASILITAS FRAGMENT
-    fun onClickEventPassData(name: String) {
-        submitFilterData()
-
-
-        println("DATA From BottomSheet to main $name")
-
-
-    }
-
-    //value selected int = 0
-    //value respond string = ""
-    private fun submitFilterData() {
-        if (valueSelected == 0){
-
-        } else {
-
-            viewModel.submitFilter(######################)
-        }
-    }
-
-    private fun initFilterData() {
-        val onFasilitas = this.resources.getStringArray(R.array.fasilitas)
-
-        for(i in onFasilitas.indices) {
-            fasilitasList.add(
-                FasilitasString(
-                    i, onFasilitas[i]
-                )
-            )
-        }
     }
 
 
